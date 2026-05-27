5c983a48f794788f5584c41832c0c6c9f3103da2
EmilliLuar <emilliluarborges@gmail.com>
2026-05-27 17:09:26 -0300

    exercicio 1 java

---
+++ script.js
@@ -0,0 +1,7 @@
+let nome1 = prompt("Olá qual seu nome ?")
+let idade1= prompt (" qual sua idade "+"(apenas numeros)")
+let endereço1 = prompt (" qual seu endereço ?")
+
+alert ( "essas informações estão corretas ? seu nome : " + nome1 + "; sua idade : "  +  idade1 + " ; seu endereço : " + endereço1 )
+
+let booleano = true
