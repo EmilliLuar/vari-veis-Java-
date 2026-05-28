1697c9fa7941e8321817c0fd212b83af584aba84
EmilliLuar <emilliluarborges@gmail.com>
2026-05-28 14:06:46 -0300

    atualização do exercício 2

--- lanche.js
+++ lanche.js
@@ -4,7 +4,7 @@ let lanche1 = prompt("Qual lanche você quer ? x-salada ; x-tudo ; x-bacon ")
 
 let bebida1 = prompt(" Qual bebida você quer ? refrigerante ; água ;  suco")
 
-alert("resumo de seu pedido: " + lanche1 +  "  "  + bebida1 )
+alert("resumo de seu pedido: " + lanche1 +  " e "  + bebida1 )
 
 alert("agradecemos sua compra, seu pedido já está em preparo ! ")
 
