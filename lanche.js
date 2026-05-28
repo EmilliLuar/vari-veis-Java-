67844a67bd071b019abb94e903075d4fed1210f8
EmilliLuar <emilliluarborges@gmail.com>
2026-05-28 13:57:26 -0300

    segundo exercício de java

---
+++ lanche.js
@@ -0,0 +1,11 @@
+alert("Caro cliente seja bem-vindo a Hamburgueria do sabor!")
+
+let lanche1 = prompt("Qual lanche você quer ? x-salada ; x-tudo ; x-bacon ")
+
+let bebida1 = prompt(" Qual bebida você quer ? refrigerante ; água ;  suco")
+
+alert("resumo de seu pedido: " + lanche1 +  "  "  + bebida1 )
+
+alert("agradecemos sua compra, seu pedido já está em preparo ! ")
+
+
